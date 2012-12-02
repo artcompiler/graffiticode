5a212ca121cfc697b5d7bdfe045a5388141bef65
Jeff Dyer <jeffdyer@acm.org>
2012-12-01 21:18:19 -0800

    update buttons

--- static/graffiticode.js
+++ static/graffiticode.js
@@ -241,8 +241,8 @@ GraffitiCode.ui = (function () {
             $("#help-view").append(data)
         })
         $.get("http://"+location.host+"/graffiti/208", function (newButton) {
-        $.get("http://"+location.host+"/graffiti/209", function (openButton) {
-        $.get("http://"+location.host+"/graffiti/210", function (saveButton) {
+        $.get("http://"+location.host+"/graffiti/240", function (openButton) {
+        $.get("http://"+location.host+"/graffiti/241", function (saveButton) {
         $.get("http://"+location.host+"/graffiti/211", function (shareButton) {
             $("#button-bar").append("<a class='button-bar-button' onclick='GraffitiCode.ui.newCode()' title='New' href='#'>"+newButton+"</a>")
             $("#button-bar").append("<a class='button-bar-button' onclick='GraffitiCode.ui.showGallery()' title='Find' href='#'>"+openButton+"</a>")
