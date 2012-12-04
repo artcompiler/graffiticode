d5bd401cd4af89e59ea54b80f1db6d18f4e5e80a
Jeff Dyer <jeffdyer@acm.org>
2012-12-04 11:23:06 -0800

    start tutorial

--- static/tutorial-starting.js
+++ static/tutorial-starting.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 		case 263:
 		    $("#coloring2-example-graffito").append(
 			"<a href='#' onclick='GraffitiCode.ui.showWorkspace(); " +
-			    "GraffitiCode.ui.updateSrc(263, \"" + data[i].src.replace("/\n/g", "\\n") + "\")'>" + 
+			    "GraffitiCode.ui.updateSrc(263, \"" + data[i].src.replace(new RegExpr("\n", "g"), "\\n") + "\")'>" + 
 			    data[i].obj +
 			    "<br/>View in workspace</a>")
 		    break
