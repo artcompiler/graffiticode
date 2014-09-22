d4adaed2e24fc4e4f33f5716d2ca888e190539e9
Jeff Dyer <jeffdyer@acm.org>
2014-09-22 12:35:48 -0700

    Fix

--- app.js
+++ app.js
@@ -295,8 +295,6 @@ function compile(language, src, response) {
 }
 
 // Compile code (idempotent)
-app.put('/code', function (req, res) {
-// Compile code (idempotent)
 app.put('/compile', function (req, res) {
   var ast = req.body.ast;
   var language = req.body.language;
