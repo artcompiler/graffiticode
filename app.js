3d6b29064ce4708978ca109930986904e026496b
Jeff Dyer <jeffdyer@acm.org>
2015-06-05 15:22:19 -0600

    Fix url

--- app.js
+++ app.js
@@ -345,10 +345,10 @@ app.get('/code', function (req, res) {
 });
 
 function retrieve(language, path, response) {
-  var port = "5" + language.substring(1);  // e.g. L103 -> 5103
-  var host = "localhost";
-//  var host = language + ".artcompiler.com";
-//  var port = "80";
+//  var port = "5" + language.substring(1);  // e.g. L103 -> 5103
+//  var host = "localhost";
+  var host = language + ".artcompiler.com";
+  var port = "80";
   var data = [];
   var options = {
     host: host,
@@ -366,10 +366,10 @@ function retrieve(language, path, response) {
 
 function compile(language, src, response) {
   // Handle legacy case
-  var host = "localhost";
-  var port = "5" + language.substring(1);  // e.g. L103 -> 5103
-//  var host = language + ".artcompiler.com";
-//  var port = "80";
+//  var host = "localhost";
+//  var port = "5" + language.substring(1);  // e.g. L103 -> 5103
+  var host = language + ".artcompiler.com";
+  var port = "80";
   var path = "/compile";
   var data = {
     "description": "graffiticode",
