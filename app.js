c2e12dc85076b806d4723d54af1f1f32002f781b
Jeff Dyer <jeffdyer@acm.org>
2015-12-21 09:55:57 -0700

    debugging

--- app.js
+++ app.js
@@ -596,9 +596,11 @@ app.post('/code', function (req, res){
         " ', '" + language + "', '" + label + "', '" + img + "');"
       client.query(queryStr, function(err, result) {
         if (err) {
+          console.log("POST /code err=" + err);
           res.status(400).send(err);
           return;
         }
+        console.log("POST /code queryStr=" + queryStr);
         var queryStr =
           "SELECT pieces.* FROM pieces ORDER BY pieces.id DESC LIMIT 1";
         client.query(queryStr, function (err, result) {
