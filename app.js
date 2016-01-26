1debf47f140e56e8b6d26db8ace855e1cce4954a
Jeff Dyer <jeffdyer@acm.org>
2016-01-26 15:51:22 -0700

    Fix crash triming source string

--- app.js
+++ app.js
@@ -522,7 +522,7 @@ function cleanAndTrimSrc(str) {
   }
   str = str.replace(new RegExp("'","g"), "''");
   while(str.charAt(0) === " ") {
-    str.shift();
+    str = str.substring(1);
   }
   while(str.charAt(str.length - 1) === " ") {
     str = str.substring(0, str.length - 1);
