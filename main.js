9c0b3bf1ee44e2d45c77ee35d0fc4a23022f8553
Jeff Dyer <jeffdyer@acm.org>
2016-05-02 21:51:10 -0600

    Add missing file

---
+++ main.js
@@ -0,0 +1,53 @@
+var parser = require('./public/parse');
+
+var nodePool
+var nodeStack
+
+var parse = exports.parse = function(src, lexicon, resume) {
+  var stream = new parser.StringStream(src);
+  var state = {
+    cc: parser.program,   // top level parsing function
+    argc: 0,
+    argcStack: [0],
+    paramc: 0,
+    paramcStack: [0],
+    env: [ {name: "global", lexicon: lexicon } ],
+    exprc: 0,
+    exprcStack: [0],
+    nodeStack: [],
+    nodePool: ["unused"],
+    nodeMap: {},
+    nextToken: -1,
+    errors: [],
+    coords: [],
+  };
+  var next = function () {
+    console.log("next() state.cc=" + state.cc);
+    return parser.parse(stream, state, resume);
+  }
+  while (state.cc != null && stream.peek()) {
+    next()
+    nodePool = state.nodePool
+    nodeStack = state.nodeStack
+    console.log("stream.peek()=" + stream.peek());
+  }
+  return nodePool;
+}
+
+process.argv.forEach(function (val, index, array) {
+  if (index < 2) return
+  
+  console.log(index + ': ' + val);
+  fs = require('fs')
+  fs.readFile(val, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("parsing: " + data);
+    var t0 = new Date;
+    parse(data, {}, function (err, ast) {
+      console.log("ast=" + JSON.stringify(ast, null, 2));
+    });
+  });
+});
