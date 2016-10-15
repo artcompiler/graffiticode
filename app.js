ca3f7484eabfe1392dac633df88e4db8da909ec9
Jeff Dyer <jeffdyer@acm.org>
2016-10-15 15:31:20 -0700

    Test redirect

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var env = process.env.NODE_ENV || 'development';
 // http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect
 app.all('*', function (req, res, next) {
   if (req.headers.host.match(/^localhost/) === null) {
-    console.log("app.all host=" + req.headers.host);
+    console.log("app.all host=" + req.headers.host + " url=" + req.url);
     if (req.headers.host === "dyerart.graffiticode.com") {
       res.redirect('https://www.graffiticode.com/form?id=471917');
     } else if (req.headers.host.match(/^www/) === null) {
