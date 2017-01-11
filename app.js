fdcdfdd91fb7eab4683aef31c1bda488422200e8
Jeff Dyer <jeffdyer@acm.org>
2017-01-11 16:04:27 -0700

    Add playground

--- app.js
+++ app.js
@@ -125,6 +125,9 @@ app.engine('html', function (templateFile, options, callback) {
 
 // http://stackoverflow.com/questions/10435407/proxy-with-express-js
 var request = require('request');
+app.get("/spokenmathplay", (req, res) => {
+  request("https://learnosity.artcompiler.com/form?id=494386").pipe(res);
+});
 app.get("/spokenmathspec", (req, res) => {
   request("https://learnosity.artcompiler.com/form?id=490914").pipe(res);
 });
