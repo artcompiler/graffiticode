31a33dce10c71575b2b59dbfb82ddca649d6df05
Jeff Dyer <jeffdyer@acm.org>
2018-07-08 08:46:57 -0700

    merge

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict'
 
@@ -36275,5 +36274,3 @@ var SourceView = React.createClass({
 exports.default = SourceView;
 
 },{"react":195,"react-dom":66}]},{},[202]);
-=======
->>>>>>> e431aa89d0ad4ee6af59211f82d34dc690738407
