091efdbbb9ed1cb7d798ad9ba6cbdae1f6445bd4
Jeff Dyer <jeffdyer@acm.org>
2019-01-27 15:44:25 -0800

    Allow remote dev database

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ if (LOCAL_DATABASE) {
 }
 
 const conStrs = [
-  LOCAL_DATABASE ? process.env.DATABASE_URL_LOCAL : process.env.DATABASE_URL,
+  LOCAL_DATABASE ? process.env.DATABASE_URL_LOCAL 
+    : DEBUG ? process.env.DATABASE_URL_DEV 
+    : process.env.DATABASE_URL,
 ];
 
 function getConStr(id) {
