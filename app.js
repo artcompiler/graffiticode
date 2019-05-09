7b5cc57bc9b47ee5be634022481bdb7405a5422e
Jeff Dyer <jeffdyer@acm.org>
2019-05-09 14:17:24 -0700

    Remove dead code

--- app.js
+++ app.js
@@ -1920,7 +1920,6 @@ if (!module.parent) {
         });
       });
     }
-//    insert107Items();
     // recompileItems([
     // ], [], {});
     // let browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
@@ -1928,63 +1927,13 @@ if (!module.parent) {
     // ], 0, () => {
     //   browser.close();
     // });
-  //   batchCompile(authToken, [{
-  //     id: "2MgH5praFL",
-  //     data: [
-  // "a=-3",
-  // "\left(90.78,\ 98.22\right)",
-  // "\left(0\right)= 7\left(3\right)- 21",
-  // "539",
-  // "\left(3,1\right)",
-  // "m=-5",
-  // "\text{$}38.67",
-  // "4578",
-  // "r=-28",
-  // "12",
-  // "2",
-  // "8,-8",
-  // "\begin{bmatrix}-24& -18& -6\ -24& -24& -30\end{bmatrix}",
-  // "\mathrm{slope}=1",
-  // "null",
-  // "\left\{4-2\sqrt{3},4+2\sqrt{3}\right\}",
-  // "\frac{\pi}{3},\frac{2\pi}{3}",
-  // "0",
-  // "4.218",
-  // "162\pi",
-  // "q^{9 m}",
-  // "\frac{1}{3}",
-  // "\text{equation: }x+4=6\text{, }\text{solution: }x=2",
-  // "(r + 68 s)(r - s)",
-  // "25\%",
-  // "\frac{15n^4+44}{66mn^6}",
-  // "-0.183",
-  // "51",
-  // "\left(6,0\right)",
-  // "70",
-  // "0.077",
-  // "\$27467.98",
-  // "xz",
-  // "7",
-  // "\left(x-3\right)\left(x+3\right)\left(x+7\right)",
-  // "11.82",
-  // "3",
-  // "-27",
-  // "-4",
-  // "-23.6",
-  // "a=4, -6",
-  // "4.40",
-  // "\left(2, \infty\right)",
-  // "y=8x-18",
-  // "\mathrm{slope}=\frac{5}{247}",
-  // "1.282",
-  // "126",
-  // "7983",
-  // "4x+2h+5",
-  // "4",
-  // "6",
-  //  ]}], 0, null, (err, val)=>{
-  //    console.log("batchCompile() val=" + JSON.stringify(val, null, 2));
-  //  });
+    // batchCompile(authToken, [{
+    //   id: "2MgH5praFL",
+    //   data: [
+    //     "a=-3",
+    //   ]}], 0, null, (err, val)=>{
+    //     console.log("batchCompile() val=" + JSON.stringify(val, null, 2));
+    //   });
     // putComp([], clientSecret);
   });
 }
@@ -2021,69 +1970,6 @@ function putComp(data, secret, resume) {
   req.on('error', function(err) {
     console.log("[14] ERROR " + err);
     resume(err);
-  });
-}
-
-function insert107Items() {
-  var obj = [];
-  var hash = {};
-  var emptyCount = 0, eraseCount = 0;
-  fs.readFile("107dump.gc", 'utf-8', function(err, data) {
-    var items = data.split("\n");
-    insertItems(items, () => {
-      process.exit(0);
-    });
-  });
-}
-
-function insertItems(items, resume) {
-  if (items.length === 0) {
-    resume();
-  } else {
-    let item = items.shift();
-    console.log(items.length + ": " + item);
-    insertItem(item, () => {
-      insertItems(items, resume);
-    });
-  }
-}
-
-function insertItem(src, resume) {
-  var lang = "L107";
-  var ast = null;
-  var obj = "";
-  var user = 0;
-  var parent = 0;
-  var img = "";
-  var label = "new";
-  var forkID = 0;
-  var queryStr = "SELECT id, ast, obj FROM pieces WHERE language='" + lang + "' AND user_id='" + user + "' AND src = '" + cleanAndTrimSrc(src) + "'";
-  dbQuery(queryStr, (err, result) => {
-    if (err) {
-      console.log("ERROR queryStr=" + queryStr);
-      resume(err);
-    } else if (result && result.rows.length === 0) {
-      postItem(lang, src, ast, obj, user, parent, img, label, forkID, (err, result) => {
-        if (err) {
-          console.log("ERROR [2] PUT /compile err=" + err);
-          resume(400);
-        } else {
-          let langID = lang.charAt(0) === "L" ? +lang.substring(1) : +lang;
-          let codeID = result.rows[0].id;
-          let dataID = 0;
-          let ids = [langID, codeID, dataID];
-          let id = encodeID(ids);
-          recompileItem(id);
-          resume();
-        }
-      });
-    } else {
-      let rows = result.rows;
-      if (rows.length > 0 /*&& !(rows[0].ast && rows[0].obj)*/) {
-        recompileItem(result.rows[0].id);
-      }
-      resume();
-    }
   });
 }
 
