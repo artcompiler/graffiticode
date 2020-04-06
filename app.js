d639cf916ee7828888f6b59826f64d9f28d243e7
Jeff Dyer <jeffdyer@acm.org>
2020-04-05 18:13:34 -0700

    Home page

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.engine('html', function (templateFile, options, callback) {
 
 // const request = require('request');
 app.get("/", (req, res) => {
-  res.redirect('https://coronavirus.artcompiler.com/form?id=l1BUBpXg5CX');
+  res.redirect('https://coronavirus.artcompiler.com/form?id=ep1Ug3wljs5');
 });
 
 const aliases = {};
