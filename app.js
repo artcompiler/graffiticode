bb7eee4b39269580817f64517a4c3daf04454f84
Kevin Dyer <kevin.m.dyer@gmail.com>
2020-04-07 20:36:00 -0600

    Added more robust cache of api/lang assets

--- app.js
+++ app.js
@@ -63,12 +63,14 @@ app.all('*', function (req, res, next) {
   }
 });
 
+const isDev = env === 'development';
+app.use(morgan((isDev ? 'dev' : 'combined'), {
+  skip: function (req, res) { return !isDev && res.statusCode < 400 }
+}));
+
 app.set('views', __dirname + '/views');
 // app.set('public', __dirname + '/public');
 // app.set('public', __dirname + '/lib');
-app.use(morgan('combined', {
-  skip: function (req, res) { return res.statusCode < 400 }
-}));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: 100000000 }));
@@ -155,7 +157,7 @@ function parse(lang, src, resume) {
     main.parse(src, lexiconCache.get(lang), resume);
   } else {
     get(lang, 'lexicon.js', (err, data) => {
-      if (err && err.length) {
+      if (err) {
         resume(err);
       } else {
         // TODO Make lexicon JSON.
@@ -725,7 +727,7 @@ function comp(auth, lang, code, data, options, resume) {
 function parseID(id, options, resume) {
   const ids = decodeID(id);
   getPiece(ids[1], (err, item) => {
-    if (err && err.length) {
+    if (err) {
       resume(err, null);
     } else {
       // if L113 there is no AST.
@@ -734,7 +736,7 @@ function parseID(id, options, resume) {
       const src = item.src;
       if (src) {
         parse(lang, src, (err, ast) => {
-          if (err && err.length) {
+          if (err) {
             resume(err);
           } else {
             if (!ast || Object.keys(ast).length === 0) {
@@ -744,7 +746,7 @@ function parseID(id, options, resume) {
               if (ids[1] && !options.dontSave) {
                 console.log(`Saving AST for id=${id}`);
                 updatePieceAST(ids[1], user, lang, ast, (err) => {
-                  if (err && err.length) {
+                  if (err) {
                     resume(err);
                   } else {
                     resume(null, ast);
@@ -926,7 +928,7 @@ app.put('/compile', function (req, res) {
         console.log(`No AST, parsing: ${src}`);
         // No AST, try creating from source.
         parse(lang, src, (err, ast) => {
-          if (err && err.length) {
+          if (err) {
             console.log(`ERROR PUT /compile parse err=${err.message}`);
             res.sendStatus(400);
           } else {
@@ -938,7 +940,7 @@ app.put('/compile', function (req, res) {
       }
       function compile({ res, userID, lang, ast }) {
         itemToID(userID, lang, ast, (err, itemID) => {
-          if (err && err.length) {
+          if (err) {
             itemID = null;
           }
           compileInternal({ res, itemID });
@@ -1032,7 +1034,11 @@ function putCode(auth, lang, rawSrc, resume) {
   const label = "show";
   const parent = 0;
   parse(lang, rawSrc, (err, ast) => {
-    compile(ast);
+    if (err) {
+      resume(err);
+    } else {
+      compile(ast);
+    }
   });
   function compile(ast) {
     const forkID = 0;
@@ -1099,7 +1105,7 @@ app.put('/code', (req, res) => {
       const label = body.label;
       const parent = body.parent ? body.parent : 0;
       parse(lang, src, (err, ast) => {
-        if (err && err.length) {
+        if (err) {
           console.log(`ERROR PUT /code parse err=${err.message}`);
           res.sendStatus(500);
         } else {
@@ -1237,18 +1243,17 @@ function num2dot(num) {
   return d;
 }
 
-const assetCache = {};
-app.get("/:lang/*", function (req, res) {
+const assetCache = new Map();
+const assetCacheTtlMs = 5 * 60 * 1000;
+app.get('/:lang/*', (req, res) => {
   // /L106/lexicon.js
   const lang = req.params.lang;
   const path = req.url;
-  let data;
-  if (!LOCAL_COMPILES && (data = assetCache[path])) {
-    res.send(data);
+  if (!LOCAL_COMPILES && assetCache.has(path)) {
+    res.send(assetCache.get(path));
   } else {
     pingLang(lang, (pong) => {
       if (pong) {
-        const chunks = [];
         const options = {
           host: getAPIHost(lang),
           port: getAPIPort(lang),
@@ -1256,15 +1261,21 @@ app.get("/:lang/*", function (req, res) {
         };
         const protocol = LOCAL_COMPILES && http || https;
         protocol.get(options, (apiRes) => {
+          const chunks = [];
           apiRes
             .on('error', (err) => {
-              console.log(`ERROR GET /:lang/* api call err=${err.message}`);
+              console.log(`ERROR GET /${lang}/* api call err=${err.message}`);
               res.sendStatus(500);
             })
             .on('data', (chunk) => chunks.push(chunk))
             .on('end', () => {
-              const data = assetCache[path] = chunks.join('');
-              res.send(data);
+              const data = chunks.join('');
+              // Only save if request is not an error
+              if (apiRes.statusCode < 400) {
+                assetCache.set(path, data);
+                setTimeout(() => assetCache.delete(path), assetCacheTtlMs);
+              }
+              res.status(apiRes.statusCode).send(data);
             });
         });
       } else {
