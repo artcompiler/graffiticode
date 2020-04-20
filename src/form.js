3e9ca9543b0bdc0a1eafb9071b4c8a44e331ecbf
Jeff Dyer <jeffdyer@acm.org>
2020-04-20 08:17:05 -0700

    Fix cross domain error

--- src/form.js
+++ src/form.js
@@ -3,9 +3,14 @@ import * as ReactDOM from "react-dom";
 import GraffView from "./graff-view";
 import Dispatcher from "./Dispatcher";
 // This is the one and only dispatcher. It is used by embedded views as well.
-window.gcexports.dispatcher = window.parent.gcexports && window.parent.gcexports.dispatcher
+try {
+  window.gcexports.dispatcher = window.parent.gcexports && window.parent.gcexports.dispatcher
                               ? window.parent.gcexports.dispatcher
-                              : new Dispatcher;
+    : new Dispatcher;
+} catch (x) {
+  // Catch cross domain error.
+  window.gcexports.dispatcher = new Dispatcher;
+}
 ReactDOM.render(
   React.createElement(GraffView, null),
   document.getElementById('graff-view')
--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import ObjectView from "./obj-view";
 import ArchiveView from "./archive-view";
 import Dispatcher from "./Dispatcher";
 // This is the one and only dispatcher. It is used by embedded views as well.
-window.gcexports.dispatcher = window.parent.gcexports && window.parent.gcexports.dispatcher
+try {
+  window.gcexports.dispatcher = window.parent.gcexports && window.parent.gcexports.dispatcher
                               ? window.parent.gcexports.dispatcher
-                              : new Dispatcher;
+    : new Dispatcher;
+} catch (x) {
+  // Catch cross domain error.
+  window.gcexports.dispatcher = new Dispatcher;
+}
 let dispatch = (obj => {
   window.gcexports.dispatcher.dispatch(obj);
 });
